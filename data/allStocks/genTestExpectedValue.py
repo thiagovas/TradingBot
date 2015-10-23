ad4c780801e7f0946da71db5003c0d730ecd7f92
Thiago Vieira <thiagovieiraas@gmail.com>
2015-10-23 00:34:16 -0200

    Added a README on the /data folder, and added a little documentation about the ANN (in portuguese)

---
+++ data/allStocks/genTestExpectedValue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -* coding: utf-8 -*-
+#
+# This script generates the values expected from the ANN output
+# Each line of the files generated has two fields:
+#   * Delta price - Closing price from the last day minus closing price from the current day.
+#   * Delta price (percentage) - Same thing as the field before, but in percentage terms.
+#     

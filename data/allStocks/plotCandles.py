e2f8ea8dd93e4013c1f44938d045aa9aa0ac20b6
Thiago Vieira <thiagovieiraas@gmail.com>
2015-11-03 11:01:03 -0200

    Added some files with sma and ema

---
+++ data/allStocks/plotCandles.py
@@ -0,0 +1,11 @@
+#!/usr/env/bin python
+# -*- coding: utf-8 -*-
+# By Thiago Silva
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Random test data
+all_data = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]]
+
+
---
+++ data/allStocks/trainset/est/averages/genAverages.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# By Thiago Silva
+# 
+# This script generates a bunch of files, where each file has the SMA and EMA for each stock
+# on the ../daily_candles folder.
+# 
+
+import numpy
+import talib
+import sys
+from os import listdir, makedirs
+from os.path import isfile, join, exists
+
+xDays = [5, 7, 10, 15, 21]
+path = '../daily_candles'
+
+def main():
+  files = [ f for f in listdir(path) if isfile(join(path, f)) ]
+  for f in files:
+    close = []
+    paper_file = open(path+'/'+f, 'r')
+    for line in paper_file.readlines():
+      close.append(float(line.split(',')[0]))
+    close = numpy.array(close)
+    for x in xDays:
+      sma = talib.SMA(close, timeperiod=x) # Simple Moving Average
+      ema = talib.EMA(close, timeperiod=x) # Exponential Moving Average
+      outFile = open(str(x)+'_closeSmaEma', 'w')
+      for i in range(len(sma)):
+        outFile.write(str(sma[i]))
+        outFile.write(' ')
+        outFile.write(str(ema[i]))
+        outFile.write('\n')
+      outFile.close()
+    paper_file.close()
+  
+
+if __name__ == '__main__':
+  main()

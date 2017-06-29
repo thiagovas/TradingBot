63a4a7c7fbcac77eadc22c923cc46c2cee4d2d91
Thiago V. de A. Silva <thiagovieiraas@gmail.com>
2017-06-28 21:21:08 -0300

    Adding more variables to the network.

--- data/treat_data.py
+++ data/treat_data.py
@@ -9,20 +9,44 @@
 # The output of this code is a file with all series joined into
 # one file. Each line representing a day, following the schema:
 #
-# Date  [DR 1]  [VaR 1]  [DR 2]  [VaR 2] ... [DR N] [VaR N]
+# Date  [DR 1_1]  [VaR 1_1]  [DR 1_2]  [VaR 1_2]  [DR 2_1]  [VaR 2_1] ... [DR N_K] [VaR N_K]
 #
 # DR    = Daily Return
-# DR _  = Daily Return of time series _
-# VaR calculated at 5%.
+# DR N_K  = Daily Return of time series N lagged of K days
+# VaR calculated at 5%, and it's saved in logarithmic scale.
 #
 # Output file saved at "./treated" folder.
 
 
 import math
 from sys import argv
+from math import log, fabs
 from scipy.stats import norm
 import numpy as np
 
+
+
+def get_full_ts(ts):
+  '''
+    TODO(thiagovas): Write Me
+  '''
+  r_ts = {}
+  n_days = 5
+  v_tmp = []
+  for key in sorted(ts):
+    value = ts[key]
+    v_tmp.extend(value)
+    
+    if len(v_tmp) > n_days*2:
+      v_tmp = v_tmp[:-2]
+    
+    if len(v_tmp) < n_days*2:
+      continue
+    
+    r_ts[key] = v_tmp
+  
+  return r_ts 
+  
 
 
 def read_ts(input_file):
@@ -33,7 +57,7 @@ def read_ts(input_file):
     It Returns a dictionary of dates and daily returns.
   '''
   first_line = True
-  
+ 
   ts = {}
   for l in input_file.readlines():
     if first_line:
@@ -45,55 +69,65 @@ def read_ts(input_file):
     except ValueError:
       continue
 
-  tmp = 0
+  tmp = 1
   r_ts = {}
   returns = []
   for key in sorted(ts):
     value = ts[key]
-    returns.append(value-tmp)
+    returns.append(float(value-tmp)/tmp)
+    if len(returns) < 30:
+      tmp = value
+      continue
+    
     if len(returns) > 90:
       del returns[0]
     mu = np.mean(returns)
     sigma = np.std(returns)
-    valueAtRisk = norm.ppf(0.05, mu, sigma)
-    
+    valueAtRisk = log(norm.ppf(0.05, mu, sigma)+1000)
+
     neue_key = key
-    
+
     if neue_key[0] == "\"" or neue_key[0] == "\'":
       neue_key = neue_key[1:]
-    
+
     if neue_key[-1] == "\"" or neue_key[-1] == "\'":
       neue_key = neue_key[:-1]
-    
+ 
     if not math.isnan(valueAtRisk):
-      r_ts[neue_key] = [value-tmp, valueAtRisk]
+      r_ts[neue_key] = [returns[-1], valueAtRisk]
     tmp = value
-  
-  return r_ts
+
+  return get_full_ts(r_ts)
 
 
 
 def join_ts(main_ts, other_ts):
+  '''
+    TODO(thiagovas): Write Me
+  '''
   if main_ts == None:
     return other_ts
-  
+
   tmp_ts = main_ts
   joint_ts = {}
-  
+
   for key, value in other_ts.items():
     if key in tmp_ts:
       tmp_ts[key].extend(value)
-  
+
   for key, value in tmp_ts.items():
     if key in other_ts:
       joint_ts[key] = value
-  
+
   return joint_ts
-  
+
 
 def write_output(output_filename, joint_ts):
+  '''
+    TODO(thiagovas): Write Me
+  '''
   ofile = open(output_filename, 'w')
-  
+
   for key in sorted(joint_ts):
     value = joint_ts[key]
     ofile.write(key)
@@ -104,8 +138,11 @@ def write_output(output_filename, joint_ts):
 
 
 def write_bayesian_output(bayesian_output_filename, joint_ts):
+  '''
+    TODO(thiagovas): Write Me
+  '''
   ofile = open(bayesian_output_filename, 'w')
-  
+
   for key in sorted(joint_ts):
     value = joint_ts[key]
     ofile.write("0 qid:12")
@@ -123,11 +160,11 @@ def main():
   if len(argv) == 1:
     print "Error: Inform at least one filename."
     return
-	
+
   joint_ts = None
   output_filename = "./treated/joint"
   bayesian_output_filename = "./treated/joint_bayesian"
-  
+ 
   fs = argv
   fs = fs[1:]
   
@@ -138,7 +175,7 @@ def main():
   
   write_output(output_filename, joint_ts)
   write_bayesian_output(bayesian_output_filename, joint_ts)
-	
+
 
 
 if __name__ == '__main__':
